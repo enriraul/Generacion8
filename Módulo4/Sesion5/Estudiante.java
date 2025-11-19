e78daf5dc9e102097e469f9c64ec97a304d86a59
Enrique Raúl OR <128866748+enriraul@users.noreply.github.com>
2025-11-18 18:02:58 -0600

    Add files via upload

---
+++ Módulo4/Sesion5/Estudiante.java
@@ -0,0 +1,46 @@
+public class Estudiante extends Persona{
+
+    private String matricula;
+    private String Facultad;
+    private int semestre;
+
+
+    public Estudiante(String nombre, String apellido, int edad, 
+    String curp, String matricula, String Facultad, int semestre){
+
+        super(nombre, apellido, edad, curp);
+        this.matricula = matricula;
+        this.Facultad = Facultad;
+        this.semestre = semestre;
+
+    }
+
+    public void info(){
+        String informacion = String.format("""
+            Información del alumno:
+            =======================
+
+            Informacion: class Persona
+
+            Nombre: %s
+            Apellido: %s
+            Edad: %d
+            Curp: %s
+
+            ======================
+
+            Informacion: class Estudiante
+
+
+            Matricula: %s
+            Facultad: %s
+            Semestre: %d
+
+            ======================
+
+
+        """,this.getNombre(), this.apellido, this.getEdad(), 
+        this.getCurp(), this.matricula, this.Facultad, this.semestre);
+        System.out.println(informacion);
+    }
+}
---
+++ Módulo4/Sesion5/Main.java
@@ -0,0 +1,15 @@
+public class Main{
+    public static void main(String[] args) {
+        Estudiante e1 = new Estudiante("Enrique","Rodríguez",33,"Curp","OURE123","Ciencias",6);
+        e1.info();
+
+    }
+
+    /*
+    
+        Persona -> Autor 
+        Generos
+        info()
+        
+     */
+}
